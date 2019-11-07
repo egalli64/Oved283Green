1b5f926318b1985f29a335b7e4bd638d02ad4d60
Your Name <poli.nadia94@gmail.com>
2019-11-07 17:14:53 +0100

    1714

---
+++ jQueryEffects/flip.js
@@ -0,0 +1,23 @@
+// $(function () {
+//     $("#card").flip({
+//         axis: 'x',
+//         trigger: 'hover',
+//         reverse: true
+//       });
+// });
+
+//$(function(){
+    // $(".card").flip({
+    //     trigger: 'hover'
+    // });
+//});
+
+
+    $(function(){
+    	$("#flip").flip({
+  			trigger: 'hover'
+		});
+  
+    
+    });
+  	
