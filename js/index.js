bfd77b66bb6e2fd64da98ebb350fb027cc6e9297
Your Name <poli.nadia94@gmail.com>
2019-11-08 16:48:20 +0100

    1648

--- js/index.js
+++ js/index.js
@@ -1,39 +1,58 @@
-var slideIndex = 0;
+window.onload = showSlidesAuto;
+
+let slideIndex = { ind: 0 };
+
+let i = { cont: 0 };
+let slides = document.getElementsByClassName("mySlides");
+
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideIndex.ind += n);
+
+
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(slideIndex.ind = n);
 }
 
-//showSlides();
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByClassName("dot");
-    /*if (plusSlides()){    
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
-else{*/
-    
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+
+//Automatico;
+
+let timer;
+function showSlidesAuto() {
+
+    for (i.cont = 0; i.cont < slides.length; i.cont++) {
+        slides[i.cont].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
-} 
+    slideIndex.ind++;
+    if (slideIndex.ind > slides.length) {
+        slideIndex.ind = 1;
+    }
+    slides[slideIndex.ind - 1].style.display = "block";
+    timer=setTimeout(showSlidesAuto, 5000); // Change image every 5 seconds
+};
+
+//manuale
+
+function showSlides(n) {
+    clearTimeout(timer);
+    timer=setTimeout(showSlidesAuto, 5000);
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex.ind = 1;
+    }
+    if (n < 1) {
+        slideIndex.ind = slides.length;
+    }
+    for (i.cont = 0; i.cont < slides.length; i.cont++) {
+        slides[i.cont].style.display = "none";
+    }
+    for (i.cont = 0; i.cont < dots.length; i.cont++) {
+        dots[i.cont].className = dots[i.cont].className.replace(" active", "");
+    }
+    slides[slideIndex.ind - 1].style.display = "block";
+    dots[slideIndex.ind - 1].className += " active";
+};
